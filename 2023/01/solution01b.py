0e13b68beb15e1c8ccb4849b07709d67bf0498b1
Lena Kalleske <lena.kalleske@f-i-sp.de>
2023-12-01 18:02:53 +0100

    Day 1

--- 2023/01/solution01b.py
+++ 2023/01/solution01b.py
@@ -1,12 +1,48 @@
 import re
 
-pattern = re.compile(r'[\d]')
+digit_map = {
+    '0': 0,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    'zero': 0,
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9
+}
+
+pattern = re.compile(r'[\d]|zero|one|two|three|four|five|six|seven|eight|nine')
+
+
+def find_last_digit(line: str, start):
+    pos = start
+    while pos >= 0:
+        all_digits = pattern.findall(line, pos)
+        if len(all_digits) > 1:
+            print('something went wrong')
+        if len(all_digits) == 1:
+            return all_digits[0]
+        pos -= 1
+
+
 sum = 0
 with open('input', 'r') as file:
     for line in file:
         digits = pattern.findall(line)
-        first_digit = int(digits[0])
-        last_digit = int(digits[-1])
+        first_digit = digit_map.get(digits[0])
+        last_digit = digit_map.get(find_last_digit(line, len(line) - 1))
         two_digit_number = 10 * first_digit + last_digit
         sum += two_digit_number
         print(f"{first_digit}, {last_digit}, {line}")
