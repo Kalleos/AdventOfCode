29c5589056573b8af347f78e9d805f012efbc7cd
Lena Kalleske <lena@kalleske.de>
2023-12-18 18:41:55 +0100

    Day 17

---
+++ 2023/17/solution17b.py
@@ -0,0 +1,37 @@
+import heapq
+
+
+def visit(matrix):
+    dists = {}
+    to_visit = [(0, 0, 0, 0, 0)]
+    while len(to_visit) > 0:
+        dist, i, j, dir, same = heapq.heappop(to_visit)
+        if (i, j, dir, same) in dists:
+            continue
+
+        dists[(i, j, dir, same)] = dist
+
+        for new_dir, (dr, dc) in enumerate([(0, 1), (1, 0), (0, -1), (-1, 0)]):
+            if new_dir != dir and 1 <= same < 4:
+                continue
+            new_i = i + dr
+            new_j = j + dc
+            new_same = 1 if new_dir != dir else same + 1
+            is_back = (dir - new_dir) % 4 == 2
+            if 0 <= new_i < len(matrix) and 0 <= new_j < len(matrix[0]) and not is_back and new_same <= 10:
+                heapq.heappush(to_visit, (dist + matrix[new_i][new_j], new_i, new_j, new_dir, new_same))
+
+    result = min([dist for (i, j, _, _), dist in dists.items() if i == len(matrix) - 1 and j == len(matrix[0]) - 1])
+    print(result)
+
+
+def solve(filename):
+    with (open(filename) as file):
+        lines = file.read().splitlines()
+        matrix = [[int(c) for c in line] for line in lines]
+        visit(matrix)
+
+
+if __name__ == '__main__':
+    solve('test_input')
+    solve('input')
