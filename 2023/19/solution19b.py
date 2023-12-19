ac4211ac777fc2d2c15607c28372596f1cc47ad6
Lena Kalleske <lena@kalleske.de>
2023-12-19 15:13:10 +0100

    Day 19 Part II

--- 2023/19/solution19b.py
+++ 2023/19/solution19b.py
@@ -1,3 +1,6 @@
+import math
+
+
 def read_workflow(line: str):
     rs = line.index('{')
     re = line.index('}')
@@ -9,43 +12,40 @@ def read_workflow(line: str):
     return name, rules
 
 
-def is_accepted(part, workflows):
-    next_workflow = 'in'
-    while next_workflow not in ['A', 'R']:
-        for rule in workflows[next_workflow]:
-            if isinstance(rule, str):
-                next_workflow = rule
-                break
-            op = rule[1]
-            if op == None:
-                next_workflow = rule[3]
-                break
-            if op == '<' and part[rule[0]] < rule[2] or op == '>' and part[rule[0]] > rule[2]:
-                next_workflow = rule[3]
-                break
-    if next_workflow == 'A':
-        return True
-    return False
+def visit(workflows, wf_name, ivs):
+    if wf_name == 'R':
+        return 0
+    if wf_name == 'A':
+        return math.prod([b - a for (a, b) in ivs.values()])
 
+    wf = workflows[wf_name]
 
-def simplify(workflows: dict[str, list[tuple[any]] | str]):
-    simplified = False
-    for name in workflows:
-        if isinstance(workflows[name], str):
+    ivs_rest = ivs.copy()
+    result = 0
+    for rule in wf:
+        op = rule[1]
+        next_wf = rule[3]
+        if op is None:
+            result += visit(workflows, next_wf, ivs_rest)
             continue
-        if all([end == 'A' for (_, _, _, end) in workflows[name]]):
-            workflows[name] = 'A'
-            simplified = True
-            continue
-        if all([end == 'R' for (_, _, _, end) in workflows[name]]):
-            workflows[name] = 'R'
-            simplified = True
-            continue
-        for i, (a, b, c, next_name) in enumerate(workflows[name]):
-            if next_name not in ['A', 'R'] and isinstance(workflows[next_name], str):
-                workflows[name][i] = (a, b, c, workflows[next_name])
-                simplified = True
-    return simplified
+        var = rule[0]
+        n = rule[2]
+        (a, b) = ivs_rest[var]
+        if op == '<' and n > a:
+            new_ivs = ivs_rest.copy()
+            new_ivs[var] = (a, min(n, b))
+            ivs_rest[var] = (min(n, b), b)
+            result += visit(workflows, next_wf, new_ivs)
+
+        if op == '>' and n < b:
+            new_ivs = ivs_rest.copy()
+            new_ivs[var] = (max(a, n) + 1, b)
+            ivs_rest[var] = (a, max(a, n) + 1)
+            result += visit(workflows, next_wf, new_ivs)
+
+        if any([b - a < 1 for (a, b) in ivs_rest.values()]):
+            break
+    return result
 
 
 def solve(filename):
@@ -60,38 +60,8 @@ def solve(filename):
             name, rules = read_workflow(line)
             workflows[name] = rules
 
-        #print(workflows)
-        for n in range(10000):
-            if not simplify(workflows):
-                print(f'Simplify nach {n} Runden abgeschlossen.')
-                break
-        #print(workflows)
-
-        bps = {'x': [1, 4001], 'm': [1, 4001], 'a': [1, 4001], 's': [1, 4001]}
-        for rules in workflows.values():
-            if isinstance(rules, str):
-                continue
-            for rule in rules:
-                op = rule[1]
-                if op == '<':
-                    bps[rule[0]].append(rule[2])
-                if op == '>':
-                    bps[rule[0]].append(rule[2] + 1)
-
-        for c in bps:
-            bps[c] = sorted(set(bps[c]))
-
-        print(bps)
-        result = 0
-        for i, x in enumerate(bps['x'][:-1]):
-            for j, m in enumerate(bps['m'][:-1]):
-                for k, a in enumerate(bps['a'][:-1]):
-                    for l, s in enumerate(bps['s'][:-1]):
-                        part = {'x': x, 'm': m, 'a': a, 's': s}
-                        if is_accepted(part, workflows):
-                            result += (bps['x'][i + 1] - bps['x'][i]) * (bps['m'][j + 1] - bps['m'][j]) * (bps['a'][k + 1] - bps['a'][k]) * (bps['s'][l + 1] - bps['s'][l])
-
-        print(result)
+        ivs = {'x': (1, 4001), 'm': (1, 4001), 'a': (1, 4001), 's': (1, 4001)}
+        print(visit(workflows, 'in', ivs))
 
 
 if __name__ == '__main__':
